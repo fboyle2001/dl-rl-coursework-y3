988d648f2908d2e8f9db0ae6edf062d53b275bce
fboyle2001 <finlayboyle2001@gmail.com>
2022-01-26 23:04:23 +0000

    Rewriting agents, SAC

--- rl-mb/main.py
+++ rl-mb/main.py
@@ -10,15 +10,19 @@ import math
 import os
 import matplotlib.pyplot as plt
 
+
+def store_memory(self, state: np.ndarray, action: np.ndarray, reward: float, next_state: np.ndarray, is_terminal: bool) -> None:
+    """
+    Store a replay if this agent supports experience replays
+    """
+    raise NotImplementedError("Agent does not support experience replays")
+    
 ## Parameters
 # General
 seed = 42
 device = "cpu" #torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-selected_agent = "PrioritisedTD3"
+selected_agent = "StandardSAC"
 fixed_time = time.time()
-
-# Agent
-warmup_steps = 0
 
 # Environment
 environment_name = "BipedalWalker-v3"
@@ -72,7 +76,8 @@ agents = {
             "warmup_function": lambda episode: max(0, 300 - episode)
         }),
         "training": {
-            "priority_buffer": False
+            "priority_buffer": False,
+            "pre_training_warmup": 0
         }
     },
     "PrioritisedTD3": {
@@ -90,16 +95,42 @@ agents = {
             "warmup_function": lambda episode: max(0, 300 - episode)
         }),
         "training": {
-            "priority_buffer": True
+            "priority_buffer": True,
+            "pre_training_warmup": 0
+        }
+    },
+    "StandardSAC": {
+        "agent": agents.StandardSACAgent,
+        "parameters": DotMap({
+            "buffer_size": 1000000,
+            "gradient_update_steps": 1,
+            "lr": 3e-4,
+            "tau": 0.005,
+            "gamma": 0.99,
+            "start_steps": 10000,
+            "replay_batch_size": 256,
+            "target_update_interval": 1,
+            "warmup_function": lambda episode: 0,
+            "log_interval": 10
+        }),
+        "training": {
+            "priority_buffer": False,
+            "pre_training_warmup": 10000
         }
     }
 }
 
 # Initialise the selected agent
-agent = agents[selected_agent]["agent"](obs_dim, act_dim, device, agents[selected_agent]["parameters"])
+agent = agents[selected_agent]["agent"](obs_dim, act_dim, device, agents[selected_agent]["parameters"], env)
+
+if selected_agent == "StandardSAC":
+    agent.run()
+
+exit()
 
 # Complete warmup steps to get information about environment if needed
-if warmup_steps != 0:
+if agents[selected_agent]["training"]["pre_training_warmup"] != 0:
+    warmup_steps = agents[selected_agent]["training"]["pre_training_warmup"]
     done = False
     accumulated_steps = 0
     state = env.reset()
@@ -114,15 +145,15 @@ if warmup_steps != 0:
             state = env.reset()
             done = False
             
-            action = env.action_space.sample()
-            next_state, reward, done, _ = env.step(action)
+        action = env.action_space.sample()
+        next_state, reward, done, _ = env.step(action)
 
-            if agents[selected_agent]["training"]["priority_buffer"]:
-                agent.store_memory(state, action, reward, next_state, done, None)
-            else:
-                agent.store_memory(state, action, reward, next_state, done)
-                
-            accumulated_steps += 1
+        if agents[selected_agent]["training"]["priority_buffer"]:
+            agent.store_memory(state, action, reward, next_state, done, None)
+        else:
+            agent.store_memory(state, action, reward, next_state, done)
+            
+        accumulated_steps += 1
     
     print("Completing final run...")
 
@@ -144,6 +175,100 @@ else:
 # Timing
 total_times = dict()
 episode_start_time = None
+
+for episode in range(1, max_episodes + 1):
+    if episode % 10 == 0:
+        training_log = open(f"{log_folder}training-log-{episode // 10}.txt", "w+")
+    
+    # Time logging
+    training_log.flush()
+    log(f"Summary of time shares so far:", episode, 0)
+    total_acc_time_sum = sum(total_times.values())
+    log(f"Total accumulated time is {total_acc_time_sum}s", episode, 0)
+
+    for key in total_times.keys():
+        pp = 100 * total_times[key] / total_acc_time_sum
+        log(f"{key}: {total_times[key]:.4f}s ({pp:.1f}%)", episode, 0)
+        
+    print(f"Starting episode {episode}")
+    eps_start_time = time.time()
+    state = env.reset()
+    warmup_it = agent.parameters.warmup_function(episode) # max(0, int(1000 * (math.log(1000) - math.log(agent.warmup_iters)) / math.log(1000))) #max(0, agent.warmup_iters - episode)
+    is_eval = episode % plot_interval == 0
+
+    #epsilon_greedy = max(reverse_sigmoid(episode, 0.18, 20), reverse_sigmoid(episode, 0.08, 20))
+    epsilon_greedy = 0 if episode >= 250 else 1 - (1 / (1 + math.exp(-0.06 * (episode - 80))))
+    
+    t_time = time.time()
+    accumulated_times = dict()
+
+    for t in range(max_timesteps):
+        if len(accumulated_times.keys()) != 0:
+            total_time = sum([x[0] for x in accumulated_times.values()]) + 1e-5
+            log(f"Total time on last step was {total_time}s", episode, t)
+
+            for key in accumulated_times.keys():
+                if key not in total_times.keys():
+                    total_times[key] = 0
+                    total_times[key] += accumulated_times[key][0]
+                    percentage = 100 * accumulated_times[key][0] / total_time
+                    log(f"{key}: {accumulated_times[key][0]:.4f}s ({percentage:.1f}%) (Info: {accumulated_times[key][1]})", episode, t)
+        
+        accumulated_times = dict()
+        log(f"Starting new timestep, spent {time.time() - t_time}s on previous", episode, t)
+        t_time = time.time()
+
+        track_time = time.time()
+        action = agent.sample_action(state)
+        accumulated_times["sampling"] = [time.time() - track_time, f"Policy sample: {t >= warmup_it}"]
+
+        track_time = time.time()
+        # take action in environment and get r and s'
+        next_state, reward, done, _ = env.step(action)
+        accumulated_times["env_step"] = [time.time() - track_time, None]
+
+        track_time = time.time()
+
+        if agents[selected_agent]["training"]["priority_buffer"]:
+            agent.store_memory(state, action, reward, next_state, done, None)
+        else:
+            agent.store_memory(state, action, reward, next_state, done)
+
+        accumulated_times["store_replay"] = [time.time() - track_time, None]
+
+        state = next_state
+        ep_reward += reward
+        
+        if t==(max_timesteps-1):
+            print("Max timesteps")
+
+        # stop iterating when the episode finished
+        if done or t==(max_timesteps-1):
+            break
+    
+    agent.train()
+    
+    # append the episode reward to the reward list
+    reward_list.append(ep_reward)
+
+    # do NOT change this logging code - it is used for automated marking!
+    log_f.write('episode: {}, reward: {}\n'.format(episode, ep_reward))
+    log_f.flush()
+    ep_reward = 0
+    
+    # print reward data every so often - add a graph like this in your report
+    if is_eval:
+        # disp.clear_output(wait=True)
+        plot_data.append([episode, np.array(reward_list).mean(), np.array(reward_list).std()])
+        reward_list = []
+        # plt.rcParams['figure.dpi'] = 100
+        plt.plot([x[0] for x in plot_data], [x[1] for x in plot_data], '-', color='tab:grey')
+        plt.fill_between([x[0] for x in plot_data], [x[1]-x[2] for x in plot_data], [x[1]+x[2] for x in plot_data], alpha=0.2, color='tab:grey')
+        plt.xlabel('Episode number')
+        plt.ylabel('Episode reward')
+        plt.savefig(f"{graph_folder}graph-{episode}.png")
+
+exit()
 
 for episode in range(1, max_episodes + 1):
     if episode % 10 == 0:
