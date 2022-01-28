9ef7b56fefa45ba6aabfff1796eab5495b94716a
fboyle2001 <finlayboyle2001@gmail.com>
2022-01-28 09:17:37 +0000

    SAC

--- rl-mb/agents.py
+++ rl-mb/agents.py
@@ -1,4 +1,4 @@
-from sys import intern
+import re
 from typing import Any, Dict, Optional, Union, Callable
 import abc
 import time
@@ -288,21 +288,16 @@ class StandardTD3Agent(RLAgent):
         for param, target_param in zip(self.actor.parameters(), self.target_actor.parameters()):
             target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
 
-a = StandardTD3Agent("BipedalWalker-v3", "cpu", 10, warmup_function=lambda episode: max(0, 300 - episode))
-a.run()
-
-class StandardSACAgent(RLAgent):
+class StandardSAC(RLAgent):
     def __init__(self, env_name: str, device: Union[str, torch.device], video_every: Optional[int],
-                    buffer_size: int = int(1e6), lr: float = 3e-4, tau: float = 0.005,
-                    replay_batch_size: int = 256, gamma: float = 0.99, gradient_steps: int = 1,
-                    min_warmup_steps: int = 10000, target_update_interval: int = 2,
-                    warmup_function: Callable[[int], int] = lambda step: 0):
+                        buffer_size: int = int(1e6), lr: float = 0.0003, tau: float = 0.005,
+                        replay_batch_size: int = 256, gamma: float = 0.99, gradient_steps: int = 1,
+                        warmup_steps: int = 10000, target_update_interval: int = 1):
         """
-        TD3 Implementation
+        SAC Implementation
         """
-        super().__init__("StandardSAC", env_name, device, video_every)
+        super().__init__("SACRewrite", env_name, device, video_every)
 
-        # Replay buffer
         self.replay_buffer = buffers.StandardReplayBuffer(self._state_dim, self._action_dim, buffer_size, self.device)
 
         # Critics predicts the reward of taking action A from state S
@@ -310,89 +305,213 @@ class StandardSACAgent(RLAgent):
         self.critic_2 = networks.Critic(input_size=self._state_dim + self._action_dim, output_size=1).to(self.device)
 
         # Actor predicts the action to take based on the current state
-        self.actor = networks.Actor(input_size=self._state_dim, output_size=self._action_dim).to(self.device)
+        # Though in SAC it actually predicts the distribution of actions
+        self.actor = networks.GaussianActor(self._state_dim, self._action_dim)
 
         # Establish the target networks
         self.target_critic_1 = copy.deepcopy(self.critic_1).to(self.device)
-        self.target_critic_1.eval()
         self.target_critic_2 = copy.deepcopy(self.critic_2).to(self.device)
-        self.target_critic_2.eval()
-        self.target_actor = copy.deepcopy(self.actor).to(self.device)
-        self.target_actor.eval()
 
-        # Establish optimisers
-        self.critic_1_opt = torch.optim.Adam(self.critic_1.parameters(), lr=lr)
-        self.critic_2_opt = torch.optim.Adam(self.critic_2.parameters(), lr=lr)
-        self.actor_opt = torch.optim.Adam(self.actor.parameters(), lr=lr)
+        # Defined in the paper as -dim(A)
+        # This line is taken from elsewhere
+        self.target_entropy = -torch.prod(torch.Tensor(self.env.action_space.shape).to(self.device)).item() # type: ignore
 
-        # Automatic temperature optimisation
-        self.target_entropy = -np.prod(self.env.action_space.shape) # type: ignore
+        # Alpha can get very close to 0 so for the same reason as in
+        # the GaussianActor we optimise ln(alpha) instead for numerical stability
+        self.log_alpha = torch.zeros(1, requires_grad=True).to(self.device)
+        self.alpha = self.log_alpha.exp() # Or 0.2?
 
+        # Optimisers
+        self.opt_critic_1 = torch.optim.Adam(self.critic_1.parameters(), lr=lr)
+        self.opt_critic_2 = torch.optim.Adam(self.critic_2.parameters(), lr=lr)
+        self.opt_actor = torch.optim.Adam(self.actor.parameters(), lr=lr)
+        self.opt_alpha = torch.optim.Adam([self.log_alpha], lr=lr)
+        
+        # Hyperparameters
+        self.gamma = gamma 
         self.tau = tau
-        self.replay_batch_size = replay_batch_size
         self.target_update_interval = target_update_interval
-        self.gamma = gamma
+        self.replay_batch_size = replay_batch_size
+        self.warmup_steps = warmup_steps
         self.gradient_steps = gradient_steps
-        self.min_warmup_steps = min_warmup_steps
-        self.warmup_function = warmup_function
     
+    def sample_evaluation_action(self, states: np.ndarray) -> np.ndarray:
+        state = torch.FloatTensor(states).to(self.device).unsqueeze(0)
+
+        with torch.no_grad():
+            action = self.actor.compute_actions(state, stochastic=False)
+
+        return action.detach().cpu().numpy()[0]
+
+    def sample_regular_action(self, states: np.ndarray) -> np.ndarray:
+        state = torch.FloatTensor(states).to(self.device).unsqueeze(0)
+
+        with torch.no_grad():
+            action = self.actor.compute_actions(state, stochastic=True)
+
+        return action.detach().cpu().numpy()[0]
+
+    def train_policy(self) -> None:
+        # Need to fill the buffer before we can do anything
+        if self.replay_buffer.count < self.replay_batch_size:
+            return
+
+        # Firstly update the Q functions (the critics)
+        buffer_sample = self.replay_buffer.sample_buffer(self.replay_batch_size)
+
+        # Q Target
+        # For this we need:
+        # * (s_t, a_t, r_t) ~ buffer
+        # * min{1,2}(Q(s_t, a_t))
+        # * a'_t ~ policy(s_[t+1])
+        # This maps to Eq 3 and Eq 5 from Haarnoja et al. 2018
+
+        # Parameters of the target critics are frozen so don't update them!
+        with torch.no_grad():
+            target_actions = self.actor.compute_actions(buffer_sample.next_states)
+            target_input = torch.cat([buffer_sample.next_states, target_actions], dim=-1)
+
+            # Calculate both critic Qs and then take the min
+            target_Q1 = self.target_critic_1(target_input)
+            target_Q2 = self.target_critic_2(target_input)
+            min_Q_target = torch.min(target_Q1, target_Q2)
+
+            # Compute r(s_t, a_t) + gamma * E_{s_[t+1] ~ p}(V_target(s_[t+1]))
+            target_Q = buffer_sample.rewards.unsqueeze(1) + buffer_sample.terminals.unsqueeze(1) * self.gamma * min_Q_target 
+            
+            # Log for TB
+            self._writer.add_scalar("stats/target_q", target_Q.detach().cpu().mean().item(), self._steps)
+
+        # Now calculate the MSE for the critics and update parameters
+        critic_input = torch.cat([buffer_sample.states, buffer_sample.actions], dim=-1)
+
+        actual_Q1 = self.critic_1(critic_input)
+        self._writer.add_scalar("stats/critic_1", actual_Q1.detach().cpu().mean().item(), self._steps)
+        actual_Q2 = self.critic_2(critic_input)
+        self._writer.add_scalar("stats/critic_2", actual_Q2.detach().cpu().mean().item(), self._steps)
+
+        # Eq 5
+        critic_1_loss = 0.5 * F.mse_loss(actual_Q1, target_Q)
+        critic_2_loss = 0.5 * F.mse_loss(actual_Q2, target_Q)
+
+        self._writer.add_scalar("stats/critic_loss_1", critic_1_loss.detach().cpu().item(), self._steps)
+        self._writer.add_scalar("stats/critic_loss_2", critic_1_loss.detach().cpu().item(), self._steps)
+
+        self.opt_critic_1.zero_grad()
+        critic_1_loss.backward()
+        self.opt_critic_1.step()
+        
+        self.opt_critic_2.zero_grad()
+        critic_2_loss.backward()
+        self.opt_critic_2.step()
+
+        # Q functions updated, now update the policy, need:
+        # * a'_t ~ policy(s_t) (NOT s_[t+1] like in Q updates)
+        # * log(a'_t | s_t) from policy
+        # * current alpha
+        # Maps to Eq 7
+
+        actor_actions, actor_probs = self.actor.compute(buffer_sample.states)
+        real_input = torch.cat([buffer_sample.states, actor_actions], dim=-1)
+
+        # See what the critics think to the actor's prediction of the next action
+        real_Q1 = self.critic_1(real_input)
+        real_Q2 = self.critic_2(real_input)
+        min_Q = torch.min(real_Q1, real_Q2)
+
+        self._writer.add_scalar('loss/mean_Q1', real_Q1.detach().mean().item(), self._steps)
+        self._writer.add_scalar('loss/mean_Q2', real_Q2.detach().mean().item(), self._steps)
+
+        # See Eq 7 
+        actor_loss = (self.alpha * actor_probs - min_Q).mean()
+        self._writer.add_scalar('loss/actor', actor_loss.detach().item(), self._steps)
+
+        self.opt_actor.zero_grad()
+        actor_loss.backward()
+        self.opt_actor.step()
+
+        # Actor updated, now update the temperature (alpha), need:
+        # * a'_t ~ policy(s_t) (NOT s_[t+1] like in Q updates)
+        # * log(a'_t | s_t) from policy
+        # * target entropy H
+        # * current alpha
+
+        # No need to backprop on the target or log probabilities since
+        # we want to isolate the alpha update here
+        alpha_loss = -self.log_alpha.exp() * (actor_probs + self.target_entropy).detach()
+        alpha_loss = alpha_loss.mean()
+        self._writer.add_scalar("loss/alpha", alpha_loss.detach().item(), self._steps)
+
+        self.opt_alpha.zero_grad()
+        alpha_loss.backward()
+        self.opt_alpha.step()
+
+        # Make sure to update alpha now we've updated log_alpha
+        self.alpha = self.log_alpha.exp()
+
+        self._writer.add_scalar('stats/alpha', self.alpha.detach().item(), self._steps)
+
+        # Update the frozen critics
+        self.update_target_parameters()
+
+    def update_target_parameters(self):
+        # Update frozen targets, taken from https://github.com/sfujim/TD3/blob/385b33ac7de4767bab17eb02ade4a268d3e4e24f/TD3.py
+        # theta' = tau * theta + (1 - tau) * theta'
+        for param, target_param in zip(self.critic_1.parameters(), self.target_critic_1.parameters()):
+            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+        for param, target_param in zip(self.critic_2.parameters(), self.target_critic_2.parameters()):
+            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
     def run(self) -> None:
-        super().run()
-
-        if self.min_warmup_steps != 0:
-            self.log(f"Have {self.min_warmup_steps} steps")
-            done = False
-            state = self.env.reset()
-            
-            while self._warmup_steps < self.min_warmup_steps or not done:
-                if done:
-                    state = self.env.reset()
-                    done = False
-                
-                action = self.env.action_space.sample()
-                next_state, reward, done, _ = self.env.step(action)
-                self.replay_buffer.store_replay(state, action, reward, next_state, done)
-
-                self._warmup_steps += 1
-            
-            self.log(f"Complete warmup steps")
-
-        while self._episodes <= self._max_episodes:
+        while self._episodes < self._max_episodes:
             self._episodes += 1
-
-            state = self.env.reset()
             done = False
-            warmup_steps = self.warmup_function(self._episodes)
-            episode_reward = 0
+            state = self.env.reset()
             self._episode_steps = 0
-
-            while not done:
-                if self._episode_steps < warmup_steps:
+            episode_reward = 0
+            
+            while not done: 
+                if self._steps < self.warmup_steps:
                     action = self.env.action_space.sample()
                 else:
                     action = self.sample_regular_action(state)
+            
+                for _ in range(self.gradient_steps):
+                    self.train_policy()
                 
                 next_state, reward, done, _ = self.env.step(action)
-                self.replay_buffer.store_replay(state, action, reward, next_state, done)
-
                 self._steps += 1
                 self._episode_steps += 1
                 episode_reward += reward
-                state = next_state
 
-                if self._episode_steps >= warmup_steps:
-                    for _ in range(self.gradient_steps):
-                        self.train_policy()
-
-            self._reward_log.append(episode_reward)
+                self.replay_buffer.store_replay(state, action, reward, next_state, done)
+                state = next_state 
+            
             self._writer.add_scalar("stats/reward", episode_reward, self.episodes)
             self.log(f"Episode Reward: {episode_reward}")
 
-    def sample_evaluation_action(self, states: np.ndarray) -> np.ndarray:
-        return np.array([])
+            if self._episodes % 10 == 0:
+                self.log("Evaluating...")
+                eval_episodes = 10
+                avg_reward = 0
 
-    def sample_regular_action(self, states: np.ndarray) -> np.ndarray:
-        return np.array([])
+                for _ in range(eval_episodes):
+                    state = self.env.reset()
+                    ep_reward = 0
+                    done = False
 
-    def train_policy(self) -> None:
-        pass
+                    while not done:
+                        action = self.sample_evaluation_action(state)
+                        next_state, reward, done, _ = self.env.step(action)
+                        ep_reward += reward
+                        state = next_state
+                    
+                    avg_reward += ep_reward
+                
+                avg_reward /= eval_episodes
+                self._writer.add_scalar("stats/eval_reward", avg_reward, self.episodes)
+                self.log(f"[EVAL] Average reward over {eval_episodes} was {avg_reward}")
+
+s = StandardSAC("BipedalWalker-v3", "cpu", None)
+s.run()
--- rl-mb/networks.py
+++ rl-mb/networks.py
@@ -2,7 +2,6 @@ from typing import List, Optional, Callable, Tuple
 
 import torch 
 import torch.nn as nn
-import torch.optim as optim
 
 import numpy as np
 
@@ -52,16 +51,111 @@ def create_fully_connected_network(input_size: int, output_size: int, hidden_lay
     return layers
 
 class Critic(nn.Sequential):
-    def __init__(self, input_size, output_size, connected_size=256):
-        super(Critic, self).__init__(
+    def __init__(self, input_size: int, output_size: int, connected_size: int = 256):
+        super().__init__(
             *create_fully_connected_network(input_size, output_size, [connected_size, connected_size], output_activation=None)
         )
 
 class Actor(nn.Sequential):
-    def __init__(self, input_size, output_size, connected_size=256):
-        super(Actor, self).__init__(
+    def __init__(self, input_size: int, output_size: int, connected_size: int = 256):
+        super().__init__(
             *create_fully_connected_network(input_size, output_size, [connected_size, connected_size], output_activation="tanh")
         )
+
+class GaussianActor(nn.Module):
+    def __init__(self, state_dims: int, action_dims: int, connected_size: int = 256, sigma_upper_bound: float = 8, action_scale: float = 1, action_offset: float = 0):
+        """
+        The Gaussian Actor takes in a state and predicts the parameters of
+        a Normal distribution for each dimension in the action dimensions.
+        This provides some stochasticisty for the actions. As such, the output
+        of the network is the mean and standard deviation of the Normals.
+
+        For numerical stability we predict the natural logarithm of the 
+        standard deviations since as σ -> 0, ln(σ) -> -inf providing improvements
+        for the stability
+        """
+        super().__init__()
+
+        self.action_scale = action_scale
+        self.action_offset = action_offset
+
+        # If the environment does not have actions in the range [-1, 1]
+        # then we will never be able to get those outside the bounds since
+        # we squash using tanh (R -> [-1, 1]) thus we may need to do a linear
+        # transform to resolve this. For BipedalWalker this is fine
+            
+        # Stability
+        self._epsilon = 1e-8
+
+        # Clip the sigmas to prevent too great variation, essentially arbitrary
+        self._log_sigma_lower_bound = torch.FloatTensor([np.log(self._epsilon)])
+        self._log_sigma_upper_bound = torch.FloatTensor([np.log(8)])
+
+        print(self._log_sigma_lower_bound)
+        print(self._log_sigma_upper_bound)
+
+        self.layers = create_fully_connected_network(state_dims, action_dims * 2, [connected_size, connected_size], output_activation=None)
+
+    def forward(self, states: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        # NN output
+        forward_pass = self.layers(states)
+
+        # The forward_pass has 2 * A elements so split down the middle
+        # autograd will handle figuring out which ones to backprop on
+        mu, log_sigma = torch.chunk(forward_pass, 2, dim=-1)
+
+        # Clamp the sigmas, can't do in-place due to autograd
+        log_sigma = log_sigma.clamp(self._log_sigma_lower_bound, self._log_sigma_upper_bound)
+        
+        # Map the log sigmas to the real sigmas
+        sigma = log_sigma.exp()
+        
+        # These are now the real parameters for the Normal distribution
+        return mu, sigma
+
+    def _compute_actions(self, states: torch.Tensor, stochastic: bool = True) -> Tuple[torch.Tensor, torch.distributions.distribution.Distribution, torch.Tensor]:
+        mu, sigma = self.forward(states)
+
+        # Need to apply the reparameterisation trick
+        dist = torch.distributions.Normal(mu, sigma)
+        unbounded_actions = dist.rsample()
+
+        # If we don't want any stochasticity in our actions
+        # e.g. when evaluating the agent then we can just take the mean
+        # of the Normal to be the action since this is the expectation
+        # of the policy over that action dimension
+
+        if stochastic:
+            actions = torch.tanh(unbounded_actions)
+        else:
+            actions = torch.tanh(mu)
+
+        # Squash into the range [-action_scale + action_offset, action_scale + action_offset]
+        actions = self.action_scale * actions + self.action_offset
+
+        return actions, dist, unbounded_actions
+
+    def compute(self, states: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        actions, dist, unbounded_actions = self._compute_actions(states)
+
+        # Appendix C of Haaraja 2018
+        log_prob_actions = dist.log_prob(unbounded_actions)
+        # For actions where x -> inf so tanh(x) -> 1, log(1 - 1) = undefined 
+        # so log(1 - 1 + eps) stabilises the calculation
+        correction = torch.log(1 - actions.square() + self._epsilon)
+        log_prob_actions -= correction
+
+        # At this point we have a tensor of the shape [B, dim(A)] and want [B, 1]
+        log_prob_actions = log_prob_actions.sum(dim=-1).unsqueeze(1)
+        return actions, log_prob_actions
+
+    def compute_actions(self, states: torch.Tensor, stochastic: bool = True) -> torch.Tensor:
+        actions, _, _ = self._compute_actions(states, stochastic)
+        return actions
+        
+    def compute_actions_log_probability(self, states: torch.Tensor) -> torch.Tensor:
+        _, log_prob_actions = self.compute(states)
+        return log_prob_actions
 
 class SACGaussianPolicy(nn.Module):
     # TODO: Understand what these actually mean
@@ -94,10 +188,11 @@ class SACGaussianPolicy(nn.Module):
         xs = normals.rsample()
         actions = torch.tanh(xs)
 
-        # Entropies
+        # Entropies, think I need to remove the minus
         log_probs = normals.log_prob(xs) - torch.log(1 - actions.pow(2) + self.eps)
+        log_probs = log_probs.sum(1, keepdim=True)
         entropies = -log_probs.sum(dim=1, keepdim=True)
 
-        return actions, entropies, torch.tanh(means)
+        return actions * 2, log_probs, torch.tanh(means) * 2
 
         
