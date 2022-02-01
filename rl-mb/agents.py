79660949d77716efae0a19d66e48343d50fae240
fboyle2001 <finlayboyle2001@gmail.com>
2022-02-01 17:26:39 +0000

    Trying SAC SVG

--- rl-mb/agents.py
+++ rl-mb/agents.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Optional, Union, Callable
+from typing import Any, Dict, Optional, Union, Callable, Tuple
 import abc
 import time
 import os
@@ -631,12 +631,12 @@ class StandardMBPOAgent(RLAgent):
 
         self.opt_critic_1.zero_grad()
         critic_1_loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.critic_1.parameters(), 1) # type: ignore
+        # torch.nn.utils.clip_grad_norm_(self.critic_1.parameters(), 1) # type: ignore
         self.opt_critic_1.step()
         
         self.opt_critic_2.zero_grad()
         critic_2_loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.critic_2.parameters(), 1) # type: ignore
+        # torch.nn.utils.clip_grad_norm_(self.critic_2.parameters(), 1) # type: ignore
         self.opt_critic_2.step()
 
         # Q functions updated, now update the policy, need:
@@ -645,20 +645,20 @@ class StandardMBPOAgent(RLAgent):
         # * current alpha
         # Maps to Eq 7
         
-        if self.dynamics.absolute_average_loss > 0.25:
-            actor_actions, actor_probs = self.actor.compute(buffer_sample.states)
-            real_input = torch.cat([buffer_sample.states, actor_actions], dim=-1)
-        else:
-            real_sample_count = int(self.env_ratio * self.replay_batch_size)
-            real_sample_indices = np.random.choice(self.replay_batch_size, real_sample_count)
-            fake_sample_count = self.replay_batch_size - real_sample_count
+        # if self.dynamics.absolute_average_loss > 10:
+        #     actor_actions, actor_probs = self.actor.compute(buffer_sample.states)
+        #     real_input = torch.cat([buffer_sample.states, actor_actions], dim=-1)
+        # else:
+        real_sample_count = int(self.env_ratio * self.replay_batch_size)
+        real_sample_indices = np.random.choice(self.replay_batch_size, real_sample_count)
+        fake_sample_count = self.replay_batch_size - real_sample_count
 
-            buffer_fake_samples = self.model_buffer.sample_buffer(fake_sample_count)
+        buffer_fake_samples = self.model_buffer.sample_buffer(fake_sample_count)
 
-            mixed_states = torch.cat([buffer_sample.states[real_sample_indices], buffer_fake_samples.states], dim=0)
+        mixed_states = torch.cat([buffer_sample.states[real_sample_indices], buffer_fake_samples.states], dim=0)
 
-            actor_actions, actor_probs = self.actor.compute(mixed_states)
-            real_input = torch.cat([mixed_states, actor_actions], dim=-1)
+        actor_actions, actor_probs = self.actor.compute(mixed_states)
+        real_input = torch.cat([mixed_states, actor_actions], dim=-1)
 
         # See what the critics think to the actor's prediction of the next action
         real_Q1 = self.critic_1(real_input)
@@ -670,7 +670,7 @@ class StandardMBPOAgent(RLAgent):
 
         self.opt_actor.zero_grad()
         actor_loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.actor.parameters(), 1) # type: ignore
+        # torch.nn.utils.clip_grad_norm_(self.actor.parameters(), 1) # type: ignore
         self.opt_actor.step()
 
         # Actor updated, now update the temperature (alpha), need:
@@ -757,7 +757,7 @@ class StandardMBPOAgent(RLAgent):
 
                 if self.steps >= self.warmup_steps:
                     # Train the policy
-                    g_steps = self.gradient_steps if self.dynamics.absolute_average_loss <= 0.25 else 1
+                    g_steps = self.gradient_steps# if self.dynamics.absolute_average_loss <= 0.25 else 1
 
                     for _ in range(g_steps):
                         self.train_policy()
@@ -967,6 +967,319 @@ class ModelBasedTD3Agent(RLAgent):
 
         for param, target_param in zip(self.actor.parameters(), self.target_actor.parameters()):
             target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+class SVGSACAgent(RLAgent):
+
+    def __init__(self, env_name: str, device: Union[str, torch.device], video_every: Optional[int],
+                    buffer_size: int = int(1e6), actor_critic_lr: float = 1e-4, alpha_lr: float = 5e-4,
+                    alpha_init: float = 0.1, tau: float = 5e-3, critic_target_update_freq: int = 1,
+                    actor_update_freq: int = 1, gamma: float = 0.99, single_step_updates: int = 1, 
+                    single_step_batch_size: int = 512, actor_log_std_lower_bound: float = -5,
+                    actor_log_std_upper_bound: float = 2, world_model_lr: float = 1e-3,
+                    multi_step_updates_n_seq: int = 4, multi_step_batch_size: int = 1024,
+                    horizon: int = 3, clip_grad: float = 1.0, dynamics_update_freq: int = 1,
+                    dynamics_update_repeat: int = 4, model_free_update_repeat: int = 4, 
+                    warmup_steps: int = 10000):
+        """
+        SVG(H) SAC Implementation
+        Other:
+            actor_detach_rho = False
+            actor_dx_threshold = None
+            actor_mve = True
+
+            critic_target_mve = False
+            full_target_mve = False
+
+
+        """
+        super().__init__("SVGSAC", env_name, device, video_every)
+
+        self.replay_buffer = buffers.StandardReplayBuffer(self._state_dim, self._action_dim, buffer_size, self.device)
+
+        # Critics predicts the reward of taking action A from state S
+        self.critic_1 = networks.Critic(input_size=self._state_dim + self._action_dim, output_size=1).to(self.device)
+        self.critic_2 = networks.Critic(input_size=self._state_dim + self._action_dim, output_size=1).to(self.device)
+
+        # Actor predicts the action to take based on the current state
+        # Though in SAC it actually predicts the distribution of actions
+        self.actor = networks.GaussianActor(self._state_dim, self._action_dim, self.device)
+
+        # Establish the target networks
+        self.target_critic_1 = copy.deepcopy(self.critic_1).to(self.device)
+        self.target_critic_2 = copy.deepcopy(self.critic_2).to(self.device)
+
+        # Defined in the paper as -dim(A)
+        # This line is taken from elsewhere
+        self.target_entropy = -torch.prod(torch.Tensor(self.env.action_space.shape).to(self.device)).item() # type: ignore
+
+        # Alpha can get very close to 0 so for the same reason as in
+        # the GaussianActor we optimise ln(alpha) instead for numerical stability
+        self.log_alpha = torch.zeros(np.log(alpha_init), requires_grad=True, device=self.device)
+
+        # World model
+        self.dynamics_model = networks.GRUDynamicsModel(self._state_dim + self._action_dim, self._state_dim, horizon,
+                                                        self.device, clip_grad=clip_grad, lr=world_model_lr)
+        self.termination_model = networks.TerminationModel(self._state_dim + self._action_dim, 1)
+        self.reward_model = networks.RewardModel(self._state_dim + self._action_dim, 1)
+
+        # Optimisers
+        self.opt_critic_1 = torch.optim.Adam(self.critic_1.parameters(), lr=actor_critic_lr)
+        self.opt_critic_2 = torch.optim.Adam(self.critic_2.parameters(), lr=actor_critic_lr)
+        self.opt_actor = torch.optim.Adam(self.actor.parameters(), lr=actor_critic_lr)
+        self.opt_alpha = torch.optim.Adam([self.log_alpha], lr=alpha_lr)
+
+        self.opt_term = torch.optim.Adam(self.termination_model.parameters(), lr=world_model_lr)
+        self.opt_reward = torch.optim.Adam(self.reward_model.parameters(), lr=world_model_lr)
+
+        # Hyperparameters
+        self.gamma = gamma
+        self.tau = tau
+        self.critic_target_update_freq = critic_target_update_freq
+        self.actor_update_freq = actor_update_freq
+        self.single_step_updates = single_step_updates
+        self.single_step_batch_size = single_step_batch_size
+        self.multi_step_updates_n_seq = multi_step_updates_n_seq
+        self.multi_step_batch_size = multi_step_batch_size
+        self.horizon = horizon
+        self.clip_grad = clip_grad
+        self.dynamics_update_freq = dynamics_update_freq
+        self.dynamics_update_repeat = dynamics_update_repeat
+        self.model_free_update_repeat = model_free_update_repeat
+        self.warmup_steps = warmup_steps
+    
+    @property
+    def alpha(self):
+        return self.log_alpha.exp()
+
+    def sample_evaluation_action(self, states: torch.Tensor) -> np.ndarray:
+        states = states.unsqueeze(0)
+
+        with torch.no_grad():
+            action = self.actor.compute_actions(states, stochastic=False)
+
+        return action.detach().cpu().numpy()[0]
+
+    def sample_regular_action(self, states: torch.Tensor) -> np.ndarray:
+        states = states.unsqueeze(0)
+
+        with torch.no_grad():
+            action = self.actor.compute_actions(states, stochastic=True)
+
+        return action.detach().cpu().numpy()[0]
+
+    def _train_reward_model(self, states, actions, expected_rewards) -> None:
+        joined = torch.cat([states, actions], dim=-1)
+        loss = F.mse_loss(self.reward_model(joined), expected_rewards)
+        self._writer.add_scalar("loss/reward", loss.detach().item(), self._steps)
+
+        self.opt_reward.zero_grad()
+        loss.backward()
+        self.opt_reward.step()
+
+    def _train_termination_model(self, states, actions, expected_termination) -> None:
+        joined = torch.cat([states, actions], dim=-1)
+        expected_termination = float(1) - expected_termination
+        n_done = torch.sum(expected_termination)
+
+        if n_done > 0:
+            pos_weight = (states.shape[0] - n_done) / n_done
+        else:
+            pos_weight = torch.tensor(1.) 
+
+        forward = self.termination_model(joined)
+        loss = F.binary_cross_entropy_with_logits(forward, expected_termination, pos_weight=pos_weight)
+        self._writer.add_scalar("loss/termination", loss.detach().item(), self._steps)
+
+        self.opt_term.zero_grad()
+        loss.backward()
+        self.opt_term.step()
+
+    def train_policy(self) -> None:
+        # Need to fill the buffer before we can do anything
+        if self.replay_buffer.count < self.single_step_batch_size:
+            return
+
+        if self.steps % self.dynamics_update_freq == 0:
+            for _ in range(self.dynamics_update_repeat):
+                trajectories = self.replay_buffer.sample_trajectories(self.multi_step_batch_size, self.multi_step_updates_n_seq)
+                states, actions, rewards = None, None, None
+                dynamics_loss = self.dynamics_model.train(trajectories[:, 0], trajectories[:, 1], trajectories[:, 2])
+
+        n_updates = 1 if self.steps < self.warmup_steps else self.model_free_update_repeat
+
+        for _ in range(n_updates):
+            # Firstly update the Q functions (the critics)
+            buffer_sample = self.replay_buffer.sample_buffer(self.single_step_batch_size)
+
+            # Q Target
+            # For this we need:
+            # * (s_t, a_t, r_t) ~ buffer
+            # * min{1,2}(Q(s_t, a_t))
+            # * a'_t ~ policy(s_[t+1])
+            # This maps to Eq 3 and Eq 5 from Haarnoja et al. 2018
+
+            # Parameters of the target critics are frozen so don't update them!
+            with torch.no_grad():
+                target_actions = self.actor.compute_actions(buffer_sample.next_states)
+                target_input = torch.cat([buffer_sample.next_states, target_actions], dim=-1)
+
+                # Calculate both critic Qs and then take the min
+                target_Q1 = self.target_critic_1(target_input)
+                target_Q2 = self.target_critic_2(target_input)
+                min_Q_target = torch.min(target_Q1, target_Q2) # should be minusing a term related to the entropy and log probabilities ?? 
+
+                # Compute r(s_t, a_t) + gamma * E_{s_[t+1] ~ p}(V_target(s_[t+1]))
+                target_Q = buffer_sample.rewards.unsqueeze(1) + buffer_sample.terminals.unsqueeze(1) * self.gamma * min_Q_target 
+                target_Q = target_Q.detach() # New?
+                
+                # Log for TB
+                self._writer.add_scalar("stats/target_q", target_Q.detach().cpu().mean().item(), self._steps)
+
+            # Now calculate the MSE for the critics and update parameters
+            critic_input = torch.cat([buffer_sample.states, buffer_sample.actions], dim=-1)
+
+            actual_Q1 = self.critic_1(critic_input)
+            self._writer.add_scalar("stats/critic_1", actual_Q1.detach().cpu().mean().item(), self._steps)
+            actual_Q2 = self.critic_2(critic_input)
+            self._writer.add_scalar("stats/critic_2", actual_Q2.detach().cpu().mean().item(), self._steps)
+
+            # Eq 5
+            critic_1_loss = 0.5 * F.mse_loss(actual_Q1, target_Q)
+            critic_2_loss = 0.5 * F.mse_loss(actual_Q2, target_Q)
+
+            self._writer.add_scalar("stats/critic_loss_1", critic_1_loss.detach().cpu().item(), self._steps)
+            self._writer.add_scalar("stats/critic_loss_2", critic_1_loss.detach().cpu().item(), self._steps)
+
+            self.opt_critic_1.zero_grad()
+            critic_1_loss.backward()
+            self.opt_critic_1.step()
+            
+            self.opt_critic_2.zero_grad()
+            critic_2_loss.backward()
+            self.opt_critic_2.step()
+
+            if self.steps % self.actor_update_freq == 0:
+                model_free_update = self.steps < self.warmup_steps
+
+                if model_free_update:
+                    # Q functions updated, now update the policy, need:
+                    # * a'_t ~ policy(s_t) (NOT s_[t+1] like in Q updates)
+                    # * log(a'_t | s_t) from policy
+                    # * current alpha
+                    # Maps to Eq 7
+
+                    actor_actions, actor_probs = self.actor.compute(buffer_sample.states)
+                    real_input = torch.cat([buffer_sample.states, actor_actions], dim=-1)
+
+                    # See what the critics think to the actor's prediction of the next action
+                    real_Q1 = self.critic_1(real_input)
+                    real_Q2 = self.critic_2(real_input)
+                    min_Q = torch.min(real_Q1, real_Q2)
+
+                    self._writer.add_scalar('loss/mean_Q1', real_Q1.detach().mean().item(), self._steps)
+                    self._writer.add_scalar('loss/mean_Q2', real_Q2.detach().mean().item(), self._steps)
+
+                    # See Eq 7 
+                    actor_loss = (self.alpha.detach() * actor_probs - min_Q).mean()
+                else:
+                    predicted_states, policy_actions, log_probs = self.dynamics_model.unroll_horizon(buffer_sample.states, self.actor)
+                    all_states = torch.cat([predicted_states, buffer_sample.states.unsqueeze(0)], dim=0)
+                    joined = torch.cat([all_states, policy_actions], dim=2)
+                    dones = self.termination_model(joined).sigmoid().squeeze(dim=2)
+                    not_dones = 1. - dones
+
+                    actor_loss = -(rewards / self.horizon).mean()
+                
+                self._writer.add_scalar('loss/actor', actor_loss.detach().item(), self._steps)
+
+                self.opt_actor.zero_grad()
+                actor_loss.backward()
+                self.opt_actor.step()
+
+                # Actor updated, now update the temperature (alpha), need:
+                # * a'_t ~ policy(s_t) (NOT s_[t+1] like in Q updates)
+                # * log(a'_t | s_t) from policy
+                # * target entropy H
+                # * current alpha
+
+                # No need to backprop on the target or log probabilities since
+                # we want to isolate the alpha update here
+                alpha_loss = (-self.alpha * (actor_probs + self.target_entropy).detach()).mean()
+                self._writer.add_scalar("loss/alpha", alpha_loss.detach().item(), self._steps)
+
+                self.opt_alpha.zero_grad()
+                alpha_loss.backward() # Could enforce some requires on the grad or decay the alpha target entropy?
+                self.opt_alpha.step()
+
+                self._writer.add_scalar('stats/alpha', self.alpha.detach().item(), self._steps)
+
+                self._train_reward_model(states, actions, rewards)
+                self._train_termination_model(states, actions, terminals) # Does this need to be 1 - terminals?
+
+                if self.steps % self.critic_target_update_freq == 0:
+                    self.update_target_parameters()
+
+
+    def update_target_parameters(self):
+        # Update frozen targets, taken from https://github.com/sfujim/TD3/blob/385b33ac7de4767bab17eb02ade4a268d3e4e24f/TD3.py
+        # theta' = tau * theta + (1 - tau) * theta'
+        for param, target_param in zip(self.critic_1.parameters(), self.target_critic_1.parameters()):
+            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+        for param, target_param in zip(self.critic_2.parameters(), self.target_critic_2.parameters()):
+            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+    def run(self) -> None:
+        while self._episodes < self._max_episodes:
+            self._episodes += 1
+            done = False
+            state = self.env.reset()
+            self._episode_steps = 0
+            episode_reward = 0
+            
+            while not done: 
+                if self._steps < self.warmup_steps:
+                    action = self.env.action_space.sample()
+                else:
+                    action = self.sample_regular_action(torch.tensor(state, device=self.device))
+            
+                self.train_policy()
+                
+                next_state, reward, done, _ = self.env.step(action)
+                self._steps += 1
+                self._episode_steps += 1
+                episode_reward += reward
+
+                store_done = done if self._steps < self.env.spec._max_episode_steps else False # type: ignore
+
+                self.replay_buffer.store_replay(state, action, reward, next_state, store_done)
+                state = next_state 
+            
+            self._writer.add_scalar("stats/reward", episode_reward, self.episodes)
+            self.log(f"Episode Reward: {episode_reward}")
+
+            if self._episodes % 10 == 0:
+                self.log("Evaluating...")
+                eval_episodes = 10
+                avg_reward = 0
+
+                for _ in range(eval_episodes):
+                    state = self.env.reset()
+                    ep_reward = 0
+                    done = False
+
+                    while not done:
+                        state = torch.tensor(state, device=self.device)
+                        action = self.sample_evaluation_action(state)
+                        next_state, reward, done, _ = self.env.step(action)
+                        ep_reward += reward
+                        state = next_state
+                    
+                    avg_reward += ep_reward
+                
+                avg_reward /= eval_episodes
+                self._writer.add_scalar("stats/eval_reward", avg_reward, self.episodes)
+                self.log(f"[EVAL] Average reward over {eval_episodes} was {avg_reward}")
 
 AGENT_MAP = {
     "MBPO": StandardMBPOAgent,
